085706cd4035521196953a605930b7dced93a2e0
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-02-29 09:53:13 +0100

    wip setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react"
+import React, { FC } from "react"
+import { Provider } from 'react-redux'
+import store from './store'
 
-export const App = () => {
+export const App = ({ children }): FC<{}> => {
 	return (
-		<div>Test</div>
+		<Provider store={store}>
+			{children}
+		</Provider>,
 	)
 }
 
---
+++ src/store/IStore.ts
@@ -0,0 +1,7 @@
+export interface IStore {
+	todos: {
+		allIds: [],
+  		byIds: {}
+	},
+	visibilityFilter: string
+}
