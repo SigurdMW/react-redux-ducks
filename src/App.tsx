3b198dd2ce93e6d7e0547503ae5258d58768de1f
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-02-29 10:05:26 +0100

    wip setup basic redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { FC } from "react"
 import { Provider } from 'react-redux'
 import store from './store'
 
-export const App = ({ children }): FC<{}> => {
+export const App: FC<{}> = ({ children }) => {
 	return (
 		<Provider store={store}>
 			{children}
-		</Provider>,
+		</Provider>
 	)
 }
 
